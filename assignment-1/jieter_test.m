063187d91e2746d04289324a9895ad409a189dfa
Jan Pieter Waagmeester <jieter@jieter.nl>
2014-09-15 09:04:48 +0200

    cleaning up a bit

--- assignment-1/jieter_test.m
+++ assignment-1/jieter_test.m
@@ -3,10 +3,6 @@
 % Jan Pieter Waagmeester
 
 % Settings
-no_inputs = 2;
-no_hidden = 3;
-no_outputs = 2;
-
 epochs = 10; 
 learning_rate = 0.1;
 
@@ -15,26 +11,34 @@ activation = @(x)(1 / (1 + exp(-x)));
 % Initializer function to assign random weights between -1 ... 1
 w_initializer = @(L)(-1 + 2 .* rand(L));
 
-% try with xor.
-features = [0, 0; 0, 1; 1, 0; 1, 1];
-targets = [0, 1; 1, 0; 1, 0; 0, 1];
+% Training set
+if 1
+    % XOR for testing purposes.
+    features = [0, 0; 0, 1; 1, 0; 1, 1];
+    targets = [0, 1; 1, 0; 1, 0; 0, 1];
+else
+    % Assignment's training data
+    features = dlmread('data/features.txt');
+    target_raw = dlmread('data/targets.txt');
 
-% Read data
-%features = dlmread('data/features.txt');
-%target_raw = dlmread('data/targets.txt');
+    % Select only a part of the input set while testing.
+    test_len = 500;
+    features = features(1:test_len,:);
+    target_raw = target_raw(1:test_len,:);
 
-% Select only a part of the input set while testing.
-%test_len = 500;
-%features = features(1:test_len,:);
-%target_raw = target_raw(1:test_len,:);
+    % Translate the nx1 size matrix to an nx(range) matrix
+    % with ones on the position for the expected output.
+    % Each row correspondents with the expected output vector.
+    targets = zeros(size(target_raw, 1), no_outputs);
+    for i=1:size(target_raw, 1)
+        targets(i, target_raw(i)) = 1;
+    end
+end
 
-% Translate the nx1 size matrix to an nx(range) matrix
-% with ones on the position for the expected output.
-% Each row correspondents with the expected output vector.
-%targets = zeros(size(target_raw, 1), no_outputs);
-%for i=1:size(target_raw, 1)
-%   targets(i, target_raw(i)) = 1;
-%end
+no_inputs = size(features, 2);
+no_hidden = 3;
+no_outputs = size(targets, 2);
+
 
 % Initialize weights for all neurons
 w_ij = w_initializer([no_inputs, no_hidden]);
