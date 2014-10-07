db11fde8afef330e1539b43a6009c7fc946117c2
Jan Pieter Waagmeester <jieter@jieter.nl>
2014-10-07 16:37:43 +0200

    some test mazes

--- assignment-3/code/maze.py
+++ assignment-3/code/maze.py
@@ -4,18 +4,24 @@
 
 class Maze(object):
 
-    def __init__(self, width, height, maze=None):
+    def __init__(self, width, height, maze=None, start=None, end=None):
         self.width = int(width)
         self.height = int(height)
 
         self.maze = maze or []
+
+        if start is not None:
+            self.set_start(start)
+
+        if end is not None:
+            self.set_end(end)
 
     def add_row(self, row):
         assert len(self.maze) <= self.height, \
             'Height mismatch, len(self.maze) = %d, height = %d, %s' % (
                 len(self.maze), self.height, self.ascii_formatted_maze()
             )
-        assert len(row) == self.width, \
+        assert len(row) >= self.width, \
             'Width mismatch: len(row) = %d != %d' % (len(row), self.width)
 
         self.maze.append(map(int, row))
@@ -26,11 +32,17 @@ class Maze(object):
 
     def set_start(self, point):
         point = map(int, point)
+        assert point[0] < self.width, 'Point exceeds width'
+        assert point[1] < self.height, 'Point exceeds height'
+
         self.start = point
         self.set_at(point, 's')
 
     def set_end(self, point):
         point = map(int, point)
+        assert point[0] < self.width, 'Point exceeds width'
+        assert point[1] < self.height, 'Point exceeds height'
+
         self.end = map(int, point)
         self.set_at(point, 'e')
 
@@ -53,6 +65,14 @@ class Maze(object):
             self.width, self.height, self.ascii_formatted_maze()
         )
 
+    def to_file(self):
+        conv = lambda x: '1' if x in (1, 's', 'e') else '0'
+        return '%d %d\n%s' % (
+            self.width,
+            self.height,
+            '\n'.join([' '.join(map(conv, x)) for x in self.maze])
+        )
+
     @staticmethod
     def from_file(filename):
         with open(filename) as f:
@@ -60,16 +80,49 @@ class Maze(object):
 
             maze = Maze(width, height)
             for row in f:
-
                 maze.add_row(row.split(' '))
 
+
+        '''Read start and end coordinates'''
         with open(filename.replace('maze.txt', 'coordinates.txt')) as f:
             maze.set_start(f.readline()[0:-2].split(','))
             maze.set_end(f.readline()[0:-2].split(','))
 
         return maze
 
+
+street_in_middle = [0] * 4 + [1] + [0] * 5
+
+loop_top = [1] * 6 + [0] * 4
+loop_row1 = [1] + [0] * 4 + [1] + [0] * 4
+loop_row2 = [0] * 5 + [1] + [0] * 3 + [1]
+testMazes = {
+    'street': Maze(
+        10, 3,
+        maze=[[0] * 10, [1] * 10, [0] * 10],
+        start=(0, 1),
+        end=(9, 1)
+    ),
+    'cross': Maze(
+        10, 10,
+        maze=[street_in_middle, street_in_middle, street_in_middle,
+              street_in_middle, [1] * 10, street_in_middle,
+              street_in_middle, street_in_middle, street_in_middle, street_in_middle],
+        start=(0, 4),
+        end=(9, 4)
+    ),
+    'loop': Maze(
+        10, 10,
+        maze=[loop_top, loop_row1, loop_row1, loop_row1, loop_row1, [1] * 10,
+              loop_row2, loop_row2, loop_row2, [0] * 5 + [1] * 5],
+        start=(0, 0),
+        end=(9, 9)
+    )
+}
+
 if __name__ == '__main__':
     print Maze.from_file('../data/easy-maze.txt')
-    print Maze.from_file('../data/medium-maze.txt')
-    print Maze.from_file('../data/hard-maze.txt')
+    # print Maze.from_file('../data/medium-maze.txt')
+    # print Maze.from_file('../data/hard-maze.txt')
+
+    print testMazes['loop'].to_file()
