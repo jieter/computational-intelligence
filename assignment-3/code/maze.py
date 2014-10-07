c1faa4cb028619a382ac84ea2d7ca041e2112bf4
Jan Pieter Waagmeester <jieter@jieter.nl>
2014-10-07 15:34:56 +0200

    also add start end end point

--- assignment-3/code/maze.py
+++ assignment-3/code/maze.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 class Maze(object):
 
     def __init__(self, width, height, maze=None):
@@ -11,14 +12,31 @@ class Maze(object):
 
     def add_row(self, row):
         assert len(self.maze) <= self.height, \
-               'Height mismatch, len(self.maze) = %d, height = %d, %s' % (len(self.maze), self.height, self.ascii_formatted_maze())
+            'Height mismatch, len(self.maze) = %d, height = %d, %s' % (
+                len(self.maze), self.height, self.ascii_formatted_maze()
+            )
         assert len(row) == self.width, \
-               'Width mismatch: len(row) = %d != %d' % (len(row), self.width)
+            'Width mismatch: len(row) = %d != %d' % (len(row), self.width)
 
-        self.maze.append(map(int, row));
+        self.maze.append(map(int, row))
+
+    def set_at(self, point, s):
+        '''Set val `s` at `point`'''
+        self.maze[point[1]][point[0]] = s
+
+    def set_start(self, point):
+        point = map(int, point)
+        self.start = point
+        self.set_at(point, 's')
+
+    def set_end(self, point):
+        point = map(int, point)
+        self.end = map(int, point)
+        self.set_at(point, 'e')
 
     def ascii_formatted_maze(self):
-        wall = lambda x: ' ' if x == 1 else '▓'
+        wall = lambda x: ' ' if x == 1 else \
+                         '▓' if x == 0 else x
         edge = '▒'
 
         # format inner walls
@@ -30,13 +48,14 @@ class Maze(object):
 
         return '\n'.join(maze)
 
-
     def __str__(self):
-        return 'Maze [%dx%d]:\n%s' % (self.width, self.height, self.ascii_formatted_maze())
+        return 'Maze [%dx%d]:\n%s' % (
+            self.width, self.height, self.ascii_formatted_maze()
+        )
 
     @staticmethod
     def from_file(filename):
-        with open(filename, 'r') as f:
+        with open(filename) as f:
             width, height = f.readline().split(' ')
 
             maze = Maze(width, height)
@@ -44,13 +63,13 @@ class Maze(object):
 
                 maze.add_row(row.split(' '))
 
-        return maze
+        with open(filename.replace('maze.txt', 'coordinates.txt')) as f:
+            maze.set_start(f.readline()[0:-2].split(','))
+            maze.set_end(f.readline()[0:-2].split(','))
 
+        return maze
 
 if __name__ == '__main__':
     print Maze.from_file('../data/easy-maze.txt')
     print Maze.from_file('../data/medium-maze.txt')
-
-
     print Maze.from_file('../data/hard-maze.txt')
-
