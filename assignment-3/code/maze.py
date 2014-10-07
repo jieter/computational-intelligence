176ef43c67f07b67acc97ca59e5cc1a325100006
Jan Pieter Waagmeester <jieter@jieter.nl>
2014-10-07 13:29:48 +0200

    test with 3 mazes

--- assignment-3/code/maze.py
+++ assignment-3/code/maze.py
@@ -3,19 +3,21 @@
 
 class Maze(object):
 
-    def __init__(self, width, height, maze=[]):
+    def __init__(self, width, height, maze=None):
         self.width = int(width)
         self.height = int(height)
 
-        self.maze = maze
+        self.maze = maze or []
 
     def add_row(self, row):
-        assert len(self.maze) <= self.height
-        assert len(row) == self.width, 'Width mismatch: len(row) = %d != %d' % (len(row), self.width)
+        assert len(self.maze) <= self.height, \
+               'Height mismatch, len(self.maze) = %d, height = %d, %s' % (len(self.maze), self.height, self.ascii_formatted_maze())
+        assert len(row) == self.width, \
+               'Width mismatch: len(row) = %d != %d' % (len(row), self.width)
 
         self.maze.append(map(int, row));
 
-    def formatted_maze(self):
+    def ascii_formatted_maze(self):
         wall = lambda x: ' ' if x == 1 else '▓'
         edge = '▒'
 
@@ -30,7 +32,7 @@ class Maze(object):
 
 
     def __str__(self):
-        return 'Maze [%dx%d]:\n%s' % (self.width, self.height, self.formatted_maze())
+        return 'Maze [%dx%d]:\n%s' % (self.width, self.height, self.ascii_formatted_maze())
 
     @staticmethod
     def from_file(filename):
@@ -39,13 +41,16 @@ class Maze(object):
 
             maze = Maze(width, height)
             for row in f:
+
                 maze.add_row(row.split(' '))
 
         return maze
 
 
 if __name__ == '__main__':
-    maze = Maze.from_file('../data/easy-maze.txt');
+    print Maze.from_file('../data/easy-maze.txt')
+    print Maze.from_file('../data/medium-maze.txt')
 
-    print maze
+
+    print Maze.from_file('../data/hard-maze.txt')
 
